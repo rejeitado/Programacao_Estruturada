ae2fde45fdd94b0f4ede11007a7b1b7eaa5a71b1
Igor Figueiredo <80803171+rejeitado@users.noreply.github.com>
2023-06-02 09:36:22 -0300

    Add files via upload

--- PE/BlocoLista.java
+++ PE/BlocoLista.java
@@ -12,10 +12,10 @@ public class BlocoLista {
         this.prox = bl;
     }
 
-    public BlocoLista getElemento(int i) {
+    public BlocoLista getElemento(int i){
         BlocoLista aux = this.prox;
         int c = 0;
-        while (aux != null) {
+        while (aux != null){
             if (c == i) {
                 return aux;
             }
@@ -24,21 +24,38 @@ public class BlocoLista {
         }
         return null;
     }
-    
-public BlocoLista removeElemento(int i){
-    BlocoLista aux = getElemento(i-1);
-    BlocoLista rem = aux.prox;
-    aux.prox = aux.prox.prox;
-    return rem;
 
-}
+    public BlocoLista removeElemento(int i){
+        BlocoLista rem = null;
+        if(this.prox == null){
+            return rem;
+        }
+
+        if(i == 0){
+            rem = this.prox;
+            this.prox = this.prox.prox;
+        } 
+
+        else{
+            BlocoLista aux = getElemento(i-1);
+            rem = aux.prox;
+            aux.prox = aux.prox.prox;
+        }
+
+        return rem;
+
+    }
+
+    public BlocoLista adicionarElemento(int i){
+        
+    }
 
     public static void main(String[] args) {
         BlocoLista bl = new BlocoLista();
         bl.add("elemento 1");
         bl.add("elemento 2");
         bl.add("elemento 3");
-        System.out.println(bl.getElemento(1));
+        System.out.println(bl.getElemento(1).elemento);
     }
 
 }
--- PE/TesteRemove.java
+++ PE/TesteRemove.java
@@ -3,13 +3,17 @@ package PE;
 public class TesteRemove {
     public static void main(String[] args) {
     BlocoLista el = new BlocoLista();
-    el.add ("elemento 1");
-    el.add ("elemento 2");
     el.add ("elemento 3");
-    BlocoLista el1 = el.getElemento(1);
+    el.add ("elemento 2");
+    el.add ("elemento 1");
+    /*BlocoLista el1 = el.getElemento(1);
     System.out.println(el1.elemento);
     el.removeElemento(1);
     el1 = el.getElemento(1);
     System.out.println(el1.elemento);
-    }
+    }*/
+    System.out.println(el.getElemento(0).elemento); /*1*/
+    el.removeElemento(0);
+    System.out.println(el.getElemento(0).elemento);
+}
 }
